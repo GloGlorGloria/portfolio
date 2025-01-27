326dd927f247fea3047ed1ec0a669317fe5d7520
GloGlorGloria <gloriabcit@gmail.com>
2025-01-27 01:39:26 -0800

    added content for Ford eAdvert project

---
+++ .history/src/app/projects/ford-poster/page_20250127004648.js
@@ -0,0 +1,139 @@
+import Image from "next/image";
+import styles from "./ford.module.css";
+import Link from "next/link";
+
+import { FaTruckPickup } from "react-icons/fa6";
+import { DiProlog } from "react-icons/di";
+
+export default function f1Poster() {
+  return (
+    <main className={styles.mainContainer}>
+      <div className={styles.contentContainer}>
+        <div className={styles.projectTitle}>
+          <h1 className={styles.project__primaryTitle} >Graphic Design <FaTruckPickup className={styles.carIcon}/></h1>
+          <div className={styles.brandIcons}>
+          <Image
+                src="/images/NationalMusuem.svg"
+                alt="National Canadian Museum of History logo"
+                width={800}
+                height={400}
+                className={styles.brandIcon}
+          />
+           <Image
+                src="/images/ford.svg"
+                alt="Ford logo"
+                width={800}
+                height={400}
+                className={styles.brandIconSmall}
+          />
+          </div>
+        </div>
+
+        {/* Project Details */}
+        <section className={styles.sectionContainer}>
+          <div className={styles.projectDetails}>
+            {/* Left Column: Large Image */}
+            <div className={styles.leftColumn}>
+              <Image
+                src="/images/Ford_station.jpg"
+                alt="Ford eAdvert in station"
+                width={800}
+                height={400}
+                className={styles.largeImage}
+              />
+            </div>
+
+            {/* Right Column: Project Details */}
+            <div className={styles.rightColumn}>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Summary</h2>
+                <p className={styles.projectSummary}>
+                This project is a digital advertisement created for the National Canadian Museum of History to promote an upcoming exhibition, "FORDward: Driving Through Time." The exhibition highlights the evolution of Ford automobiles over 121 years, showcasing the company's innovations and its impact on transportation history. 
+                </p>
+              </div>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Design Tool</h2>
+                <ul className={styles.skillsList}>
+                  <li>
+                    <Image src="/icons/Photoshop.svg" alt="Adobe Photoshop Icon" width={40} height={40} />
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Skills</h2>
+                <ul className={styles.skillsList}>
+                  <li className={styles.skillsItem}>Graphic Design</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.sectionContainer}>
+            <div className={styles.projectDetails}>
+            <div className={styles.leftColumn}>
+              <div className={styles.sections}>
+                <h1 className={styles.sectionTitle}>Design Concept <DiProlog className={styles.designIcon}/></h1>
+                <p className={styles.projectSummary}>
+                The design emphasizes the central figures—each F1 driver and their car—using high contrast and vibrant colors to draw attention and convey a sense of power and motion. To complement the dynamic foreground, the background incorporates desaturated imagery of iconic scenic spots from the respective Grand Prix locations, serving as subtle nods to the global stage of the competition.
+                </p>
+                <br></br>
+                <p className={styles.projectSummary}>
+                Neon colors, gradients, and noise effects add layers of texture and modernity, creating a striking visual style that reflects the high-tech, high-speed world of F1. These design choices aim to immerse the audience in the atmosphere of the race while celebrating the fusion of sport, culture, and artistry.
+                </p>
+              </div>
+            </div>
+            {/* Left Column: Large Image */}
+              <div className={styles.rightColumn}>
+                <Image
+                  src="/images/F1_thumbnail.png"
+                  alt="F1 posterts mockup"
+                  width={800}
+                  height={400}
+                  className={styles.largeImage}
+                />
+              </div>
+            </div>
+        </section>
+
+        {/* Additional Project Images */}
+        <div className={styles.additionalImages}>
+            <div>
+                <Image
+                  src="/images/F1_Bustop.png"
+                  alt="F1 bus stop poster"
+                  width={800}
+                  height={400}
+                  className={styles.projectImage_Special}
+                />
+            </div>
+            <div className={styles.additionalImagesGrid}>
+              <Image
+                src="/images/Max.png"
+                alt="F1-driver Max Verstappen"  
+                width={800}
+                height={400}
+                className={styles.projectImage}
+              />
+              <Image
+                src="/images/Norris revised.png"
+                alt="F1-driver Lando Norris"
+                width={800}
+                height={400}
+                className={styles.projectImage}
+              />
+              <Image
+                src="/images/Lewis.png"
+                alt="F1-driver Lewis Hamilton"
+                width={800}
+                height={400}
+                className={styles.projectImage}
+              />
+            </div>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ .history/src/app/projects/ford-poster/page_20250127013531.js
@@ -0,0 +1,143 @@
+import Image from "next/image";
+import styles from "./ford.module.css";
+import Link from "next/link";
+
+import { FaTruckPickup } from "react-icons/fa6";
+import { DiProlog } from "react-icons/di";
+
+export default function f1Poster() {
+  return (
+    <main className={styles.mainContainer}>
+      <div className={styles.contentContainer}>
+        <div className={styles.projectTitle}>
+          <h1 className={styles.project__primaryTitle} >Graphic Design <FaTruckPickup className={styles.carIcon}/></h1>
+          <div className={styles.brandIcons}>
+          <Image
+                src="/images/NationalMusuem.svg"
+                alt="National Canadian Museum of History logo"
+                width={800}
+                height={400}
+                className={styles.brandIcon}
+          />
+           <Image
+                src="/images/ford.svg"
+                alt="Ford logo"
+                width={800}
+                height={400}
+                className={styles.brandIconSmall}
+          />
+          </div>
+        </div>
+
+        {/* Project Details */}
+        <section className={styles.sectionContainer}>
+          <div className={styles.projectDetails}>
+            {/* Left Column: Large Image */}
+            <div className={styles.leftColumn}>
+              <Image
+                src="/images/Ford_station.jpg"
+                alt="Ford eAdvert in station"
+                width={800}
+                height={400}
+                className={styles.largeImage}
+              />
+            </div>
+
+            {/* Right Column: Project Details */}
+            <div className={styles.rightColumn}>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Summary</h2>
+                <p className={styles.projectSummary}>
+                This project is a digital advertisement created for the National Canadian Museum of History to promote an upcoming exhibition, "FORDward: Driving Through Time." The exhibition highlights the evolution of Ford automobiles over 121 years, showcasing the company's innovations and its impact on transportation history. 
+                </p>
+              </div>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Design Tool</h2>
+                <ul className={styles.skillsList}>
+                  <li>
+                    <Image src="/icons/Illustrator.svg" alt="Adobe Illustrator Icon" width={40} height={40} />
+                  </li>
+                  <li>
+                    <Image src="/icons/Photoshop.svg" alt="Adobe Photoshop Icon" width={40} height={40} />
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.sections}>
+                <h2 className={styles.infoTitle}>Skills</h2>
+                <ul className={styles.skillsList}>
+                  <li className={styles.skillsItem}>Graphic Design</li>
+                  <li className={styles.skillsItem}>Content Design</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.sectionContainer}>
+            <div className={styles.projectDetails}>
+            <div className={styles.leftColumn}>
+              <div className={styles.sections}>
+                <h1 className={styles.sectionTitle}>Design Concept <DiProlog className={styles.designIcon}/></h1>
+                <p className={styles.projectSummary}>
+                The eAdvert merges five iconic Ford vehicles from different eras into a cohesive visual, showcasing the evolution of Ford's designs and innovations. Arranged chronologically from the 1908 Model T to the 2024 F-150 Raptor, the vehicles symbolize Ford's enduring journey through time and its continuous drive for progress. Designed to captivate audiences across digital platforms, the ad highlights Ford's rich legacy and unwavering commitment to shaping the future of transportation.  
+                <span className={styles.slogan}> Ford keeps moving forward!</span>
+                </p>
+              </div>
+            </div>
+            {/* Left Column: Large Image */}
+              <div className={styles.rightColumn}>
+                <Image
+                  src="/images/Ford_iPad.jpg"
+                  alt="Ford eAdvert on iPad"
+                  width={800}
+                  height={400}
+                  className={styles.largeImage}
+                />
+              </div>
+            </div>
+        </section>
+
+        {/* Additional Project Images */}
+        <div className={styles.additionalImages}>
+            <div>
+                <Image
+                  src="/images/Fordward_Mac.jpg"
+                  alt="Ford poster on Macbook"
+                  width={800}
+                  height={400}
+                  className={styles.projectImage_Special}
+                />
+               
+            </div>
+            <div className={styles.additionalImagesGrid}>
+                <Image
+                  src="/images/Fordward_vertical.png"
+                  alt="Ford poster"
+                  width={800}
+                  height={400}
+                  className={styles.projectImage_Special}
+                />
+            <div className={styles.additionalImagesGridRight}>
+                <Image
+                    src="/images/fordward outdoor.png"
+                    alt="Ford poster outdoor"
+                    width={800}
+                    height={400}
+                    className={styles.projectImage}
+                />
+                <Image
+                    src="/images/Fordward_board.jpg"
+                    alt="Ford poster outdoor"
+                    width={800}
+                    height={400}
+                    className={styles.projectImage}
+                />
+            </div>
+            </div>
+        </div>
+      </div>
+    </main>
+  );
+}
